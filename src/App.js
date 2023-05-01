757f28840e08e200a1f2705c48839962881f2079
Mitch Allen <github@mitchallen.com>
2023-04-30 22:11:00 -0400

    feat: init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './demo.css';
 
 function App() {
+
+    // Define dialog display flags
+
+  const [showDialogA, setShowDialogA] = useState(false);
+  const [showDialogB, setShowDialogB] = useState(false);
+  const [showDialogC, setShowDialogC] = useState(false);
+
+    // Define function to clear all dialogs
+
+  const clearDialogs = () => {
+    setShowDialogA(false)
+    setShowDialogB(false)
+    setShowDialogC(false)
+  }
+
+    // Define handler for clicking the first button
+
+  const handleClickAlpha = (event) => {
+    clearDialogs()
+    setShowDialogA(!showDialogA)
+  };
+
+  // Define handler for clicking the second button
+
+  const handleClickBeta = (event) => {
+    clearDialogs()
+    setShowDialogB(!showDialogB)
+  };
+
+  // Define handler for clicking the third button
+
+  const handleClickGamma = (event) => {
+    clearDialogs()
+    setShowDialogC(!showDialogC)
+  };
+
+    // Define a style to control the display of the first dialog
+
+  let styleA = {
+    display: showDialogA ? 'block' : 'none',
+  }
+
+  // Define a style to control the display of the second dialog
+
+  let styleB = {
+    display: showDialogB ? 'block' : 'none',
+  }
+
+  // Define a style to control the display of the third dialog
+
+  let styleC = {
+    display: showDialogC ? 'block' : 'none',
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="pair">
+        <button class="item" onClick={handleClickAlpha}>A</button>
+        <div id="childA" class="dialog" style={styleA}></div>
+      </div>
+      <div class="pair">
+        <button class="item" onClick={handleClickBeta}>B</button>
+        <div id="childB" class="dialog" style={styleB}></div>
+      </div>
+      <div class="pair">
+        <button class="item" onClick={handleClickGamma}>C</button>
+        <div id="childC" class="dialog" style={styleC}></div>
+      </div>
     </div>
   );
 }
