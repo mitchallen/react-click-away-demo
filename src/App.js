46b977a1f19c1801c96809f24afa50d3a35a306a
Mitch Allen <github@mitchallen.com>
2023-05-01 06:26:43 -0400

    feat: added code to implement click away

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import { useState } from 'react';
+/**
+ * Author: Mitch Allen
+ * https://mitchallen.com
+ * https://scriptable.com
+ */
+
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import './demo.css';
 
 function App() {
 
-    // Define dialog display flags
+  // Define button + dialog reference pairs
+
+  const RefPair = () => ({ button: useRef(), dialog: useRef() })
+
+  const refA = RefPair();
+  const refB = RefPair();
+  const refC = RefPair();
+
+  // Define dialog display flags
 
   const [showDialogA, setShowDialogA] = useState(false);
   const [showDialogB, setShowDialogB] = useState(false);
   const [showDialogC, setShowDialogC] = useState(false);
 
-    // Define function to clear all dialogs
+  // Define function to clear all dialogs
 
   const clearDialogs = () => {
     setShowDialogA(false)
     setShowDialogB(false)
     setShowDialogC(false)
   }
+
+  // Define useEffect to handle click events
+
+  useEffect(() => {
+
+    const handleMouseDoen = e => {
+
+      let isOver = (rf) => rf.current && rf.current.contains(e.target);
+      let isOutside = (rf) => rf.current && !rf.current.contains(e.target);
+
+      let checkOutside = function (label, flag, rp) {
+        let isOverButton = isOver(rp.button);
+        let isOutsideDialog = isOutside(rp.dialog);
+        if (flag && !isOverButton && isOutsideDialog) {
+          clearDialogs();
+        }
+      }
+
+      // important to reference flags below for useEffect
+      // if a flag isn't referenced this won't work for that flag
+      checkOutside("A", showDialogA, refA);
+      checkOutside("B", showDialogB, refB);
+      checkOutside("C", showDialogC, refC);
+    }
+
+    document.addEventListener("mousedown", handleMouseDoen);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handleMouseDoen);
+    }
+
+  }, [
+    showDialogA,
+    refA,
+    showDialogB,
+    refB,
+    showDialogC,
+    refC,
+  ])
 
     // Define handler for clicking the first button
 
@@ -57,20 +111,21 @@ function App() {
     display: showDialogC ? 'block' : 'none',
   }
 
+  // Layout with refd
 
   return (
     <div className="App">
       <div class="pair">
-        <button class="item" onClick={handleClickAlpha}>A</button>
-        <div id="childA" class="dialog" style={styleA}></div>
+        <button class="item" ref={refA.button} onClick={handleClickAlpha}>A</button>
+        <div id="childA" class="dialog" ref={refA.dialog} style={styleA}></div>
       </div>
       <div class="pair">
-        <button class="item" onClick={handleClickBeta}>B</button>
-        <div id="childB" class="dialog" style={styleB}></div>
+        <button class="item" ref={refB.button} onClick={handleClickBeta}>B</button>
+        <div id="childB" class="dialog" ref={refB.dialog} style={styleB}></div>
       </div>
       <div class="pair">
-        <button class="item" onClick={handleClickGamma}>C</button>
-        <div id="childC" class="dialog" style={styleC}></div>
+        <button class="item" ref={refC.button} onClick={handleClickGamma}>C</button>
+        <div id="childC" class="dialog" ref={refC.dialog} style={styleC}></div>
       </div>
     </div>
   );
